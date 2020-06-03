0b03116afec88cdfb409e164eabe1312455df253
xiaoyining <marvinshawn9333@gmail.com>
2020-06-03 20:19:03 +0800

    update: test github actions

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@
 import { NativeModules, NativeEventEmitter, Platform } from 'react-native';
 const { IPNCrossBorder } = NativeModules;
 const IPNEmitter = new NativeEventEmitter(IPNCrossBorder);
-// const AppleEmitter = new NativeEventEmitter(ApplePay);
-
 export const ipnPay = param => {
     if (Platform.OS === 'ios') {
       return new Promise((resolve, reject) => {
